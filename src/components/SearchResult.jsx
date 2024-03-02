4998a4f8fea809f63e5134d6e4bae9cc2188e01a
Vermilion <theabyssinian@protonmail.com>
2024-03-01 20:02:27 -0600

    Changes to be committed:

--- src/components/SearchResult.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./SearchResult.css"
-
-export const SearchResult = ({result}) => {
-    return(
-        <div className="search-result" onClick={(e) => alert(`You clicked on ${result.name}`)}>{result.name}</div>
-    )
-}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,9 @@
 import { useState } from "react"
-import { SearchBar } from "../components/SearchBar"
 import "./Home.css"
-import { SearchResultsList } from "../components/SearchResultsList"
 
 export default function Home() {
-
-    const [results, setResults] = useState([])
-
     
     return(
-        <div className="search-bar-container"> 
-            <SearchBar setResults={setResults}/>
-            <SearchResultsList results={results}/>
-        </div>
+        <h1>Home</h1>
     )
 }
