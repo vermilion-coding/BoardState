b49f9a00b8162c17ba74ea71c663e339bd1765b7
Vermilion <theabyssinian@protonmail.com>
2024-03-01 18:01:59 -0600

    Changes to be committed:

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,14 @@ import { useState } from "react"
 import { SearchBar } from "../components/SearchBar"
 import "./Home.css"
 import { SearchResultsList } from "../components/SearchResultsList"
+import {SearchCard} from "../components/SearchCard"
 
 export default function Home() {
 
     const [results, setResults] = useState([])
 
-
+    
     return(
-        <div className="search-bar-container"> 
-            <SearchBar setResults={setResults}/>
-            <SearchResultsList results={results}/>
-        </div>
+        <div> <SearchCard/> </div>
     )
 }
