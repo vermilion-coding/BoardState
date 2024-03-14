e9889a1a8dc7893178c426b885b13a1d4a776588
Vermilion <theabyssinian@protonmail.com>
2024-03-14 17:07:46 -0500

    Added dynamic Decks page

--- src/pages/Decks.jsx
+++ src/pages/Decks.jsx
@@ -11,6 +11,7 @@ export default function Decks() {
     const [selectedCard, setSelectedCard] = useState(null);
     const [openDialog, setOpenDialog] = useState(false);
     const [deckCardCounts, setDeckCardCounts] = useState({});
+    const [showDeck, setShowDeck] = useState(true);
 
     useEffect(() => {
         if (searchQuery.trim() !== '') {
@@ -97,6 +98,7 @@ export default function Decks() {
     const selectDeck = (deck) => {
         setCurrentDeck(deck);
         setSelectedCards(deck.cards);
+        setShowDeck(false);
     };
 
     const saveDeck = (deck, cards) => {
@@ -147,67 +149,82 @@ export default function Decks() {
         window.URL.revokeObjectURL(url);
     };
 
+    const handleBackButtonClick = () => {
+        setShowDeck(true);
+        setCurrentDeck(null);
+        setSelectedCards([]);
+    };
+
     return (
         <Container maxWidth="lg">
             <Grid container spacing={2}>
-                <Grid item xs={12} sm={currentDeck ? 6 : 12}>
-                    <Box border={1} borderRadius={4} p={2} style={{ height: '375px', maxHeight: '400px', overflowY: 'auto' }}>
-                        <Typography variant="h4">Decks</Typography>
-                        <Button variant="contained" onClick={createNewDeck}>New Deck</Button>
-                        <Box mt={2} style={{ display: 'flex', flexDirection: 'column', maxHeight: '240px', overflowY: 'auto' }}>
-                            {decks.map(deck => (
-                                <div key={deck.id} style={{ border: '1px solid black', borderRadius: '4px', padding: '8px', marginBottom: '8px' }}>
-                                    <Typography variant="h6">{deck.name}</Typography>
-                                    <div>
-                                        <Button variant="contained" onClick={() => selectDeck(deck)} style={{ width: '80px', height: '50px' }}>Edit</Button>
-                                        <Button variant="contained" onClick={() => deleteDeck(deck.id)} style={{ width: '80px', height: '50px' }}>Delete</Button>
-                                        <Button variant="contained" onClick={() => renameDeck(deck.id)} style={{ width: '80px', height: '50px' }}>Rename</Button>
-                                        <Button variant="contained" onClick={() => exportDeck(deck)} style={{ width: '80px', height: '50px' }}>Export</Button>
+                {showDeck ? (
+                    <Grid item xs={12}>
+                        <Box border={1} borderRadius={4} p={2} style={{ height: '800px', maxHeight: '800px', overflowY: 'auto' }}>
+                            <Typography variant="h4">Decks</Typography>
+                            <Button variant="contained" onClick={createNewDeck}>New Deck</Button>
+                            <Box mt={2} style={{ display: 'flex', flexDirection: 'column', maxHeight: '670px', overflowY: 'auto' }}>
+                                {decks.map(deck => (
+                                    <div key={deck.id} style={{ border: '1px solid black', borderRadius: '4px', padding: '8px', marginBottom: '8px' }}>
+                                        <Typography variant="h6">{deck.name}</Typography>
+                                        <div>
+                                            <Button variant="contained" onClick={() => selectDeck(deck)} style={{ width: '80px', height: '50px' }}>Edit</Button>
+                                            <Button variant="contained" onClick={() => deleteDeck(deck.id)} style={{ width: '80px', height: '50px' }}>Delete</Button>
+                                            <Button variant="contained" onClick={() => renameDeck(deck.id)} style={{ width: '80px', height: '50px' }}>Rename</Button>
+                                            <Button variant="contained" onClick={() => exportDeck(deck)} style={{ width: '80px', height: '50px' }}>Export</Button>
+                                        </div>
                                     </div>
-                                </div>
-                            ))}
+                                ))}
+                            </Box>
                         </Box>
-                    </Box>
-                </Grid>
-                {currentDeck && (
-                <>
-                <Grid item xs={12} sm={6}>
-                    <Box border={1} borderRadius={4} p={2} mt={0} ml={2}>
-                        <Typography variant="h5">{`Selected Cards - ${currentDeck.name}`}</Typography>
-                        <Box mt={1} style={{ height: '300px', maxHeight: '300px', overflowY: 'auto' }}>
-                            {selectedCards.map((card, index) => (
-                                <div key={index} style={{ display: 'flex', alignItems: 'center', marginBottom: '8px', cursor: 'pointer', boxShadow: '0 4px 8px rgba(0,0,0,0.1)' }}>
-                                    <Typography style={{ width: '80px' }}>{deckCardCounts[currentDeck.id][card.id] || 0}</Typography>
-                                    <Typography onClick={() => handleCardNameClick(card)}>{card.name}</Typography>
-                                    <Button variant="contained" size="small" onClick={() => addCardToSelected(card)} style={{ width: '80px', height: '50px' }}>+</Button>
-                                    <Button variant="contained" size="small" onClick={() => removeCardFromSelected(card.id)} style={{ width: '80px', height: '50px' }}>-</Button>
-                                </div>
-                            ))}
+                    </Grid>
+                ) : (
+                    <Grid item xs={12} sm={12}>
+                        <Box border={1} borderRadius={4} p={2} style={{ display: 'flex', alignItems: 'center' }}>
+                            <Button variant="contained" onClick={handleBackButtonClick} style={{ width: '80px', height: '50px' }}>Back</Button>
+                            <Typography variant="h4" style={{ marginLeft: 'auto' }}>{currentDeck.name}</Typography>
                         </Box>
-                    </Box>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <Box border={1} borderRadius={4} p={2} mt={0} ml={2}>
-                        <TextField
-                            type="text"
-                            placeholder="Search for cards..."
-                            value={searchQuery}
-                            onChange={(e) => setSearchQuery(e.target.value)}
-                            variant="outlined"
-                            fullWidth
-                        />
-                        <Box mt={2} style={{ maxHeight: '300px', overflowY: 'auto' }}>
-                            <Typography variant="h5">Search Results:</Typography>
-                            {searchResults.map(card => (
-                                <div key={card.id} style={{ display: 'flex', alignItems: 'center', cursor: 'pointer', marginBottom: '8px', boxShadow: '0 4px 8px rgba(0,0,0,0.1)' }}>
-                                    <Typography onClick={() => handleCardNameClick(card)}>{card.name}</Typography>
-                                    <Button maxWidth='80px' variant="contained" size="small" onClick={() => addCardToSelected(card)} style={{ width: '160px', height: '50px' }}>Add</Button>
-                                </div>
-                            ))}
+                    </Grid>
+                )}
+                {!showDeck && (
+                    <Grid item xs={12} sm={5.5}>
+                        <Box border={1} borderRadius={4} p={2} mt={0} ml={2}>
+                            <Typography variant="h5">Selected Cards</Typography>
+                            <Box mt={1} style={{ height: '630px', maxHeight: '630px', overflowY: 'auto' }}>
+                                {selectedCards.map((card, index) => (
+                                    <div key={index} style={{ display: 'flex', alignItems: 'center', marginBottom: '8px', cursor: 'pointer', boxShadow: '0 4px 8px rgba(0,0,0,0.1)' }}>
+                                        <Typography style={{ width: '80px' }}>{deckCardCounts[currentDeck.id][card.id] || 0}</Typography>
+                                        <Typography onClick={() => handleCardNameClick(card)}>{card.name}</Typography>
+                                        <Button variant="contained" size="small" onClick={() => addCardToSelected(card)} style={{ width: '80px', height: '50px' }}>+</Button>
+                                        <Button variant="contained" size="small" onClick={() => removeCardFromSelected(card.id)} style={{ width: '80px', height: '50px' }}>-</Button>
+                                    </div>
+                                ))}
+                            </Box>
                         </Box>
-                    </Box>
-                </Grid>
-                </>
+                    </Grid>
+                )}
+                {!showDeck && (
+                    <Grid item xs={12} sm={6}>
+                        <Box border={1} borderRadius={4} p={2} mt={0} ml={2}>
+                            <TextField
+                                type="text"
+                                placeholder="Search for cards..."
+                                value={searchQuery}
+                                onChange={(e) => setSearchQuery(e.target.value)}
+                                variant="outlined"
+                                fullWidth
+                            />
+                            <Box mt={2} style={{ maxHeight: '600px', overflowY: 'auto' }}>
+                                <Typography variant="h5">Search Results</Typography>
+                                {searchResults.map(card => (
+                                    <div key={card.id} style={{ display: 'flex', alignItems: 'center', cursor: 'pointer', marginBottom: '8px', boxShadow: '0 4px 8px rgba(0,0,0,0.1)' }}>
+                                        <Typography onClick={() => handleCardNameClick(card)}>{card.name}</Typography>
+                                        <Button maxWidth='80px' variant="contained" size="small" onClick={() => addCardToSelected(card)} style={{ width: '160px', height: '50px' }}>Add</Button>
+                                    </div>
+                                ))}
+                            </Box>
+                        </Box>
+                    </Grid>
                 )}
             </Grid>
             <Dialog open={openDialog} onClose={handleCloseDialog}>
